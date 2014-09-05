09087f155693ddc5b12274ac3b756095b25c7bd4
AlanYu <alan21120000@gmail.com>
2014-09-05 22:15:05 +0800

    Project initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SunnyHills::Application.config.secret_key_base = '10e3d7675749c8aadb57fb67ade65bd23dde3088f4eaf33d05fa9a1fa29b668f1ba6f1649985c927073a1e3bd536107e4841f0905db10c22ed220750c3d40ebb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SunnyHills::Application.config.session_store :cookie_store, key: '_sunnyHills_session'
